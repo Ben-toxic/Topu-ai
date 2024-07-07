c4a4720ff40bc710ab7256e851289515eb84b15e
DR BEN <110499599+Ben-toxic@users.noreply.github.com>
2024-07-07 23:13:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -62,9 +62,9 @@ module.exports = {
   antilink_values: process.env.ANTILINK_VALUES || "all",
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
-  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
-  LANG: (process.env.THEME || "TOPU").toUpperCase(),
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "4a4930278cb0ed92fe3ed42f1b8f45c1",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  LANG: (process.env.THEME || "DR BEN").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
